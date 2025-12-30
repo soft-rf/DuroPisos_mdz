3e6ad65e82cfe7f2b714cf6685b7618abecf4a2c
soft.rf <rabanalfernandezrodrigo.soft@gmail.com>
2025-12-29 23:10:58 -0300

    error 404 deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
+import inject from "vite-plugin-html-inject";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
   publicDir: "../public",
-  plugins: [],
+  plugins: [inject()],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        proyectos: resolve(__dirname, "src/proyectos.html"),
+        durocrete: resolve(__dirname, "src/duro-crete.html"),
+        durofix: resolve(__dirname, "src/duro-fix.html"),
+        revoquecolor: resolve(__dirname, "src/revoque-color.html"),
+        selladores: resolve(__dirname, "src/selladores.html"),
+        faq: resolve(__dirname, "src/faq.html"),
+      },
+    },
   },
 });
